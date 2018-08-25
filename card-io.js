541a99f7cffb517900e401974e478edc95c49523
Max <massimiliano.baki@besquare.it>
2018-08-25 18:58:41 +0200

    plugin

---
+++ card-io.js
@@ -0,0 +1,58 @@
+var CardIO = null;
+
+try {
+  CardIO = UIViewController.extend({
+      setup: function() {
+        CardIOUtilities.preload();
+      },
+      init: function() {
+          var self = this.super.init();
+          if (self) {
+              // The base class initialized successfully
+              console.log("Initialized");
+          }
+
+          console.log("card IO utilities loaded");
+
+          return self;
+      },
+      scanCard: function(viewController) {
+        var self = this;
+
+        return new Promise(function (resolve, reject) {
+          self.resolve = resolve;
+          self.reject = reject;
+
+
+          var scanViewController = CardIOPaymentViewController.new().initWithPaymentDelegate(self);
+          scanViewController.delegate = self;
+          scanViewController.modalPresentationStyle = UIModalPresentationFormSheet;
+          viewController.presentViewControllerAnimatedCompletion(scanViewController,true,null);
+        });
+
+      },
+
+      userDidProvideCreditCardInfoInPaymentViewController: function(info, paymentViewController) {
+
+          paymentViewController.dismissViewControllerAnimatedCompletion(true,null);
+          if(this.resolve) this.resolve(info);
+          console.log("did receive card number:", info.redactedCardNumber);
+      },
+      userDidCancelPaymentViewController: function(paymentViewController) {
+          debugger;
+
+          console.log("cancel");
+          paymentViewController.dismissViewControllerAnimatedCompletion(true,null);
+          if(this.reject) this.reject();
+      }
+  }, {
+      name: "CardIO",
+      protocols: [CardIOPaymentViewControllerDelegate]
+  });
+
+  CardIO.ObjCProtocols = [CardIOPaymentViewControllerDelegate];
+} catch (e) {
+  console.log(e.message);
+}
+
+exports.CardIO = CardIO.new();
